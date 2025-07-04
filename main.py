7ba96676994b634170b14b3bc36b81105e4f432d
kamiichii <berkay.kilic.ce@gmail.com>
2025-07-03 23:37:00 -0400

    Added a way to get inputs from the user

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,8 +7,12 @@ from google import genai
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
+    user_input = " ".join(sys.argv[1:])
+    if user_input.strip() == "":
+        print('You need to input a string after calling the program like python main.py "my input"')
+        sys.exit(1)
+    
     response = client.models.generate_content(model ="gemini-2.0-flash-001", contents ="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
     print(response.text)
     print(f"Prompt tokens: {str(response.usage_metadata.prompt_token_count)}")
